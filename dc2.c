57a2e1f445590e7c140ea0813f3d2adcf7a507b1
Davide Mastromatteo <mastro35@gmail.com>
2025-07-30 21:03:25 +0200

    new version 0.3.0

--- dc2.c
+++ dc2.c
@@ -21,8 +21,8 @@
  */
 
 #define APP_VERSION_MAJOR 0
-#define APP_VERSION_MINOR 2
-#define APP_VERSION_PATCH 5
+#define APP_VERSION_MINOR 3
+#define APP_VERSION_PATCH 0
 
 // Helper macros to stringify values
 #define STR_HELPER(x) #x
